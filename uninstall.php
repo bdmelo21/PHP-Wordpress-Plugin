d1b30700a97fa0b6261474e94db06bb5b02fc564
Bernardo Melo <bernardodmelo@gmail.com>
2021-02-22 15:43:18 +0000

    Plugin 1.0.0

--- uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Trigger this file on Plugin uninstall
- * @package titlenameChange Plugin
- */
-
-global $wpdb;
-$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'book'");
-$wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELEECT if FROM wp_posts)");
